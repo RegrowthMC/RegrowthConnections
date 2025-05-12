72f727ba63e48d517b64d46f3a735fa7bb2e40f0
Oak <55620516+OakLoaf@users.noreply.github.com>
2025-05-12 21:27:43 +0100

    Update and rename PluginName.java to RegrowthConnections.java

--- src/main/java/org/lushplugins/pluginname/RegrowthConnections.java
+++ src/main/java/org/lushplugins/pluginname/RegrowthConnections.java
@@ -1,9 +1,9 @@
-package org.lushplugins.pluginname;
+package org.lushplugins.connections;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class PluginName extends JavaPlugin {
-    private static PluginName plugin;
+public final class RegrowthConnections extends JavaPlugin {
+    private static RegrowthConnections plugin;
 
     @Override
     public void onLoad() {
@@ -20,7 +20,7 @@ public final class PluginName extends JavaPlugin {
         // Disable implementation
     }
 
-    public static PluginName getInstance() {
+    public static RegrowthConnections getInstance() {
         return plugin;
     }
 }
